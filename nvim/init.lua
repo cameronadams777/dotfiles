e0c9bf8995558c1502a86c0b13cff10260d8ae51
rust <rustxyz@gmail.com>
2024-02-27 09:33:32 -0600

    chore: make updates to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,36 +1,13 @@
-require("cameron.plugins-setup")
-require("cameron.core.options")
-require("cameron.core.keymaps")
-require("cameron.core.colorscheme")
+require("cameron")
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  open_on_setup = false,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {}
+  sort = {
+    sorter = "case_sensitive",
   },
   view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
+    width = 30,
   },
   renderer = {
     group_empty = true,
   },
-})
-
-require('git-blame-line').setup({
-  git = {
-    default_message = 'Not committed yet',
-    blame_format = '%an - %ar - %s' -- see https://git-scm.com/docs/pretty-formats
-  },
-  view = {
-    left_padding_size = 5,
-    enable_cursor_hold = false
-  }
 })
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -79,10 +79,15 @@ _G.packer_plugins = {
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  catppuccin = {
+  ["cellular-automaton.nvim"] = {
     loaded = true,
-    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/catppuccin",
-    url = "https://github.com/catppuccin/nvim"
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/cellular-automaton.nvim",
+    url = "https://github.com/eandrju/cellular-automaton.nvim"
+  },
+  ["cloak.nvim"] = {
+    loaded = true,
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/cloak.nvim",
+    url = "https://github.com/laytan/cloak.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
@@ -109,15 +114,15 @@ _G.packer_plugins = {
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
-  },
-  ["git-blame-line.nvim"] = {
-    loaded = true,
-    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/git-blame-line.nvim",
-    url = "https://github.com/kessejones/git-blame-line.nvim"
   },
   harpoon = {
     loaded = true,
@@ -189,6 +194,17 @@ _G.packer_plugins = {
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["refactoring.nvim"] = {
+    loaded = true,
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/refactoring.nvim",
+    url = "https://github.com/theprimeagen/refactoring.nvim"
+  },
+  ["rose-pine"] = {
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
+    loaded = true,
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/rose-pine",
+    url = "https://github.com/rose-pine/neovim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -209,6 +225,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["zen-mode.nvim"] = {
+    loaded = true,
+    path = "/Users/cameron/.local/share/nvim/site/pack/packer/start/zen-mode.nvim",
+    url = "https://github.com/folke/zen-mode.nvim"
   }
 }
 
@@ -217,6 +238,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for trouble.nvim]], true)
 try_loadstring("\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
 time([[Config for trouble.nvim]], false)
+-- Config for: rose-pine
+time([[Config for rose-pine]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
+time([[Config for rose-pine]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
